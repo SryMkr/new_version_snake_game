5c3268e2df1d3a3db676cfe191ec78bae75f7800
SryMkr <73199151+SryMkr@users.noreply.github.com>
2021-11-30 16:12:43 +0000

    Add files via upload

--- Snake_food_library.py
+++ Snake_food_library.py
@@ -37,11 +37,18 @@ class MySprite_food(pygame.sprite.Sprite):
         self.last_change_time = 0
         # catch the amount of correct food
         self.food_number = 0
+        # 创建一个正确字母和错误字母的字典
+        self.letters_dic = {0: [4, 8, 14, 20, 24], 1: [3, 15, 16, 19], 2: [10, 18, 25, 19], 3: [1, 15, 16, 19],
+                            4: [0, 14, 20, 24, 8], 5: [21, 22], 6: [9, 7], 7: [12, 13], 8: [0, 8, 14, 24, 4], 9: [6, 8],
+                            10: [2, 6], 11: [17, 8], 12: [13, 7], 13: [12, 7], 14: [0, 4, 8, 20, 24],
+                            15: [3, 1, 16, 19], 16: [15, 1, 3, 19], 17: [11, 21], 18: [25, 2], 19: [3, 2], 20: [22, 21],
+                            21: [22, 20, 5], 22: [5, 21], 23: [18, 25], 24: [8, 4], 25: [18, 2]}
 
     # get the x coordinate of frame
     def _get_x_coordinate(self):
         return self.rect.x
     # set the x coordinate of frame
+
     def _set_x_coordinate(self, value):
         self.rect.x = value
     # allow the the class have x_coordinate property with two actions
@@ -51,6 +58,7 @@ class MySprite_food(pygame.sprite.Sprite):
     def _get_y_coordinate(self):
         return self.rect.y
     # set the y coordinate of frame
+
     def _set_y_coordinate(self, value):
         self.rect.y = value
     # allow the the class have y_coordinate property with two actions
@@ -65,7 +73,6 @@ class MySprite_food(pygame.sprite.Sprite):
     # allow the the class have topleft_position property with two actions
     topleft_position = property(_get_topleft_position, _set_topleft_position)
 
-
     # load main variety
     def load_multi_frames(self, multi_frames_filepath, one_frame_width, one_frame_height, multi_frames_columns):
         # load a picture with multi-frames
@@ -79,9 +86,7 @@ class MySprite_food(pygame.sprite.Sprite):
         # the picture's columns
         self.multi_frames_columns = multi_frames_columns
 
-
 # -------------------------------------------------------------------------------------------
-
     # this function is for showing correct alphabet
     def target_update(self, al_dic, current_time, change_rate, x_position, y_position):
         # continuously change frame with time
@@ -115,8 +120,6 @@ class MySprite_food(pygame.sprite.Sprite):
             self.food_number += 1
 
 # -------------------------------------------------------------------------------------------
-
-
     # this function is for showing tip
     def tip_update(self, current_time, change_rate, x_position, y_position):
         # continuously change frame with time
@@ -149,12 +152,12 @@ class MySprite_food(pygame.sprite.Sprite):
         self.image = self.multi_frames.subsurface(rect)
 
 # -------------------------------------------------------------------------------------------
-    # this function is for wrong answer 更新错误字母的单词，每个错误字母都有更新
+    # this function is for wrong answer 更新错误字母的单词，每个错误字母都有更新，改为按照一定的要求更新
     # confict_number cannot the same as correct answer
-    def random_update(self, confict_number,current_time, change_rate, x_position, y_position):
+    def random_backup(self, confict_number,current_time, change_rate, x_position, y_position):
         # randomly change frame with time
         if current_time > self.last_change_time + change_rate:
-            alphabet_list = list(range(0,26))
+            alphabet_list = list(range(0, 26))
             # remove the correct number
             alphabet_list.remove(confict_number)
             # randomly choose a alphabet
@@ -173,7 +176,27 @@ class MySprite_food(pygame.sprite.Sprite):
             # get the image surface
             self.image = self.multi_frames.subsurface(random_rect)
 
-
+    # this function is for wrong answer 更新错误字母的单词，每个错误字母都有更新，改为按照一定的要求更新
+    # confict_number cannot the same as correct answer
+    def random_update(self, confict_number, current_time, change_rate, x_position, y_position):
+        # randomly change frame with time
+        if current_time > self.last_change_time + change_rate:
+            confused_letters = self.letters_dic[confict_number]
+            # randomly choose a alphabet
+            self.random_frame = random.choice(confused_letters)
+            # random position
+            self.topleft_position = (x_position, y_position)
+            # replace last last_change_time with current time
+            self.last_change_time = current_time
+            # the x coordinate of current frame
+            current_frame_x_coordinate = (self.random_frame % self.multi_frames_columns) * self.one_frame_width
+            # the y coordinate of current frame
+            current_frame_y_coordinate = (self.random_frame // self.multi_frames_columns) * self.one_frame_height
+            # the rectangle of current frame
+            random_rect = Rect(current_frame_x_coordinate, current_frame_y_coordinate, self.one_frame_width,
+                               self.one_frame_height)
+            # get the image surface
+            self.image = self.multi_frames.subsurface(random_rect)
 
 
 
